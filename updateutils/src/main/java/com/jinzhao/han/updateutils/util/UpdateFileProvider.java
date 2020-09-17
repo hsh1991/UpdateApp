c1a4f1f1b4068d01525195efcd3aff59a93bfe24
Han <228346592@qq.com>
2020-09-17 16:10:32 +0800

    init

---
+++ updateutils/src/main/java/com/jinzhao/han/updateutils/util/UpdateFileProvider.java
@@ -0,0 +1,10 @@
+package com.jinzhao.han.updateutils.util;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by mac on 2020/9/17.
+ */
+
+public class UpdateFileProvider extends FileProvider {
+}
